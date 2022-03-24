b242cd7c13deb418a11f5302cdd29b5ebe83281e
Yasgc24 <4537@holbertonschool.com>
2022-03-23 18:51:14 -0700

    This is the main

---
+++ main.h
@@ -0,0 +1,20 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+
+typedef struct print
+{
+char *ptr;
+int (*f)(va_list);
+} print_t;
+
+int _putchar(char c);
+int _printf(const char *format, ...);
+int print_char(va_list c);
+int print_str(va_list s);
+int print_dec(va_list d);
+int print_int(va_list i);
+
+#endif
