74ea106d1643b18db8a6f39130836caf54ae6343
Yasgc24 <4537@holbertonschool.com>
2022-03-23 20:49:41 -0700

    Prints a char and a string

---
+++ printfcs.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * print_char - function that prints a char
+ * @c: character
+ * Return: 1
+ */
+
+int print_char(va_list c)
+{
+_putchar(va_arg(c, int));
+return(1);
+}
+
+/**
+ * print_str - function that prints a string
+ * @s: string
+ * Return: length of the string
+ */
+
+int print_str(va_list s)
+{
+char *string = va_arg(s, char *);
+int x;
+if (string == NULL)
+{
+string = "(null)";
+}
+x = 0;
+while (string[x])
+{
+_putchar(string[x]);
+x++;
+}
+return (x);
+}
