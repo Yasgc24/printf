078865767596832dc8f7f0521a1911b24bde2541
Yasgc24 <4537@holbertonschool.com>
2022-03-25 17:01:39 -0700

    Correction _printf

--- _printf.c
+++ _printf.c
@@ -1,13 +1,13 @@
 #include "main.h"
 /**
  * c_specifiers - check conversion specifiers
- * @format: format specifier
+ * @format1: format specifier
  * Return: Pointer or NULL
  */
 
 int (*c_specifiers(const char *format1))(va_list)
 {
-print_t ch[] = {
+print_t cs[] = {
 {"c", print_char},
 {"s", print_str},
 {"d", print_dec},
@@ -16,8 +16,54 @@ print_t ch[] = {
 };
 int i;
 i = 0;
-while (ch[i].print != NULL && *(ch[i].print) != *format1)
+while (cs[i].print != NULL && *(cs[i].print) != *format1)
 i++;
-return (ch[i].f);
+return (cs[i].f);
 }
 
+/**
+ * _printf - produces output according to a format
+ * @format: format
+ * Return:  number of characters printed
+ */
+
+int _printf(const char *format, ...)
+{
+va_list list;
+int a = 0, count = 0;
+int (*f)(va_list);
+
+if (format == NULL)
+return (-1);
+
+va_start(list, format);
+
+while (format[a] != '\0')
+{
+if (format[a] == '%' && format[a + 1] == '\0')
+{
+_putchar('%');
+count++;
+return (count);
+}
+else if (format[a] == '%' && format[a + 1] == '%')
+{
+_putchar('%');
+a += 2;
+count++;
+}
+else if (format[a] == '%')
+{
+f = c_specifiers(&format[a + 1]);
+if (f != NULL)
+a += 2;
+count += f(list);
+}
+else
+_putchar(format[a]);
+a++;
+count++;
+}
+va_end(list);
+return (count);
+}
